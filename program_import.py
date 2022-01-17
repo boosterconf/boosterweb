a7cdabf57f0531c30e4bd748473ce8d2239673f0
Håkon Heggernes Lerring <hakon@lerring.no>
2022-01-17 22:23:04 +0100

    Map talk types of our spreadsheet to mor readable talk types Misc hugo config

--- program_import.py
+++ program_import.py
@@ -18,6 +18,12 @@ def create_speaker_file(speaker_name):
         file.write('# twitter_handle: \n')
         file.write('---\n')
 
+talk_type_map = {
+    'Workshop 3 hours': "Workshop (3 hours)",
+    'Experience report 30 min': "Experience report (30 min)",
+    'Workshop 1.5 hour': "Workshop (1.5 hours)",
+    'Lightning talk 10 min': "Lightning talk"
+}
 with open('talks.csv', 'rt', encoding="utf-8") as csvfile:
     reader = csv.reader(csvfile, quotechar='"')
     for row in reader:
@@ -27,18 +33,21 @@ with open('talks.csv', 'rt', encoding="utf-8") as csvfile:
         copresentername = row[3]
         title = row[6]
         abstract = row[7]
+        talk_type = row[8]
         akseptert = row[24]
         if akseptert == "x":
             with open("content/talk/" + number + "-" + string_to_filename(title) + ".md", "w+") as file:
                 file.truncate(0)
                 file.write('---\n')
                 file.write(f'title: "{title}"\n')
+                file.write(f'talk_type: "{talk_type_map[talk_type]}"\n')
                 file.write('authors:\n')
                 file.write(f'    - {navn}\n')
                 if copresentername != "":
                     file.write(f'    - {copresentername}\n')
 
                 file.write('---\n')
+                file.write(abstract + "\n")
             create_speaker_file(navn)
             if copresentername != "":
                 create_speaker_file(copresentername)
